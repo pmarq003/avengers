cd821fd764fed5c5a920bc26527d9d29783a16aa
mediocregopher <bgp2396@bellsouth.net>
2013-02-06 18:21:56 -0500

    more cleanup

--- avengers.py
+++ avengers.py
@@ -1,6 +1,6 @@
 import os, sys
 
-from player import CaptainAmerica
+import player
 from eventmanager import EventManager
 
 import pygame
@@ -17,26 +17,20 @@ mSPF = 1000.0/float(FPS)
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
+bgcolor = 36, 48, 59
+
 #creates window
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Avengers')
-red = 255, 0, 0
-green = 0, 255, 0
-blue = 0, 0, 255
-white = 255, 255, 255
-black = 0, 0, 0
-bgcolor = 36, 48, 59
-screen.fill(bgcolor)
 
 logo = pygame.image.load('300x300logo.jpg')
 logorect = logo.get_rect()
 toCenter = 150
 logorect.topleft = (SCREEN_WIDTH/2-toCenter,SCREEN_HEIGHT/2-toCenter)
 
-player1 = CaptainAmerica(SCREEN_HEIGHT - 100)
+player1 = player.CaptainAmerica(SCREEN_HEIGHT - 100)
 
 fontObj = pygame.font.Font('freesansbold.ttf', 100)
-msg = 'Avengers'
 
 evman = EventManager()
 
@@ -47,7 +41,7 @@ while True:
     player1.update(evman)
 
     msg = player1.message
-    msgSurface = fontObj.render(msg, False, black)
+    msgSurface = fontObj.render(msg, False, (0,0,0))
     msgRect = msgSurface.get_rect()
     msgRect.topleft = (0,0)
 
@@ -60,5 +54,3 @@ while True:
     milliEnd = pygame.time.get_ticks()
     leftover = mSPF - (milliEnd - milliStart)
     if leftover > 0: pygame.time.wait(int(leftover))
-
-clock.tick(30)
