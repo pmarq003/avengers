66ba79d8eea700ba219338a86545d6f20ccbeb32
Pablo Marquez <pmarq003@ufl.edu>
2013-02-10 17:31:22 -0500

    fixed responsiveness of start button. quit button refuses to work

--- startmenu.py
+++ startmenu.py
@@ -28,8 +28,8 @@ splash = splash.convert()
 splash.fill((0,0,0))
 x, y = screen.get_size()
 screen.blit(splash, (0, 0))
-logo = pygame.image.load("images/splash.png").convert_alpha()
-screen.blit(logo, (0,0))
+image = pygame.image.load("images/splash.png").convert_alpha()
+screen.blit(image, (0,0))
 pygame.display.update()
 
 start = Button(369, 363, 122, 22, "images/menusprites/startgame.png")
@@ -40,20 +40,42 @@ volume = Button(970, 0, 25, 25, "images/menusprites/volume.png")
 pygame.display.update()
 
 playing = False
+vol = True
 
-while(not pygame.event.peek(pygame.MOUSEBUTTONDOWN)):
-	pygame.event.wait()
+while(not playing):
+	for event in pygame.event.get():
+		if event.type == MOUSEBUTTONDOWN:
+			if event.button == 1:
+				if pygame.Rect(start.left, start.top, start.width, start.height).collidepoint(pygame.mouse.get_pos()):
+					playing = True
+					print "start hit"
 
-for event in pygame.event.get():
-	if event.type == MOUSEBUTTONDOWN:
-		if event.button == 1:
+				elif pygame.Rect(instructions.left, instructions.top, instructions.width, instructions.height).collidepoint(pygame.mouse.get_pos()):
+					image = pygame.image.load("images/instructions.png")
+					screen.blit(image, (0,0))
+					back = Button(414, 500, 173, 22, "images/back.png")
+					pygame.display.update()
+					while(not pygame.event.peek(pygame.MOUSEBUTTONDOWN)):
+						pygame.event.wait()
+						for event in pygame.event.get():
+							if pygame.Rect(back.left, back.top, back.width, back.height).collidepoint(pygame.mouse.get_pos()):
+								image = pygame.image.load("images/splash.png").convert_alpha()
+								screen.blit(image, (0,0))
+								start = Button(369, 363, 122, 22, "images/menusprites/startgame.png")
+								instructions = Button(367, 393, 115, 22, "images/menusprites/instructions.png")
+								options = Button(383, 423, 85, 22, "images/menusprites/options.png")
+								quit = Button(371, 453, 12, 22, "images/menusprites/quit.png")
+								volume = Button(970, 0, 25, 25, "images/menusprites/volume.png")
+								pygame.display.update()
 
-			playing = pygame.Rect(start.left, start.top, start.width, start.height).collidepoint(pygame.mouse.get_pos())
+				elif pygame.Rect(options.left, options.top, options.width, options.height).collidepoint(pygame.mouse.get_pos()):
+					print "options hit"
 
-			inst = pygame.Rect(instructions.left, instructions.top, instructions.width, instructions.height).collidepoint(pygame.mouse.get_pos())
-
-			ops = pygame.Rect(options.left, options.top, options.width, options.height).collidepoint(pygame.mouse.get_pos())
-
-			quits = pygame.Rect(quit.left, quit.top, quit.width, quit.height).collidepoint(pygame.mouse.get_pos())
-
-			vol = pygame.Rect(volume.left, volume.top, volume.width, volume.height).collidepoint(pygame.mouse.get_pos())
+				elif pygame.Rect(quit.left, quit.top, quit.width, quit.height).collidepoint(pygame.mouse.get_pos()):
+					print "quit hit"
+					
+				elif pygame.Rect(volume.left, volume.top, volume.width, volume.height).collidepoint(pygame.mouse.get_pos()):
+					volume.img = "images/menusprites/volume.png"
+					pygame.display.update()
+					vol = not vol
+					print vol
