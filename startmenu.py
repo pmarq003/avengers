c54466257aae64420ed7f2fc6e83ede82ad03771
Pablo Marquez <pmarq003@ufl.edu>
2013-02-10 16:22:21 -0500

    cleaned up

--- startmenu.py
+++ startmenu.py
@@ -1,10 +1,22 @@
 import pygame
-import levelobject
 import time
 
 from pygame.locals import *
 from pygame.sprite import Sprite
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT
+
+class Button:
+	def __init__(self, left, top, width, height, img):
+		self.left = left
+		self.top =top
+		self.width = width
+		self.height = height
+		self.img = img
+		button = pygame.Surface((width,height), pygame.SRCALPHA, 32)
+		button = button.convert_alpha()
+		screen.blit(button, (left,top))
+		image = pygame.image.load(img)
+		screen.blit(image,(left,top))
 
 #creates window
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -20,35 +32,11 @@ logo = pygame.image.load("images/splash.png").convert_alpha()
 screen.blit(logo, (0,0))
 pygame.display.update()
 
-startbutton = pygame.Surface((112,22), pygame.SRCALPHA, 32)
-startbutton = startbutton.convert_alpha()
-screen.blit(startbutton, (369,363))
-button0 = pygame.image.load("images/menusprites/startgame.png")
-screen.blit(button0,(369,363))
-
-instructions = pygame.Surface((115,22), pygame.SRCALPHA, 32)
-instructions = instructions.convert_alpha()
-screen.blit(instructions, (367,393))
-button1 = pygame.image.load("images/menusprites/instructions.png")
-screen.blit(button1,(367,393))
-
-options = pygame.Surface((85,22), pygame.SRCALPHA, 32)
-options = options.convert_alpha()
-screen.blit(options, (383,423))
-button2 = pygame.image.load("images/menusprites/options.png")
-screen.blit(button2,(383,423))
-
-quit = pygame.Surface((112,22), pygame.SRCALPHA, 32)
-quit = quit.convert_alpha()
-screen.blit(quit, (371, 453))
-button3 = pygame.image.load("images/menusprites/quit.png")
-screen.blit(button3,(371,453))
-
-volume = pygame.Surface((25,25), pygame.SRCALPHA, 32)
-volume = volume.convert_alpha()
-screen.blit(volume, (970, 0))
-button4 = pygame.image.load("images/menusprites/volume.png")
-screen.blit(button4,(970,0))
+start = Button(369, 363, 122, 22, "images/menusprites/startgame.png")
+instructions = Button(367, 393, 115, 22, "images/menusprites/instructions.png")
+options = Button(383, 423, 85, 22, "images/menusprites/options.png")
+quit = Button(371, 453, 12, 22, "images/menusprites/quit.png")
+volume = Button(970, 0, 25, 25, "images/menusprites/volume.png")
 pygame.display.update()
 
 playing = False
@@ -60,17 +48,12 @@ for event in pygame.event.get():
 	if event.type == MOUSEBUTTONDOWN:
 		if event.button == 1:
 
-			srect = pygame.Rect(369,363,112,22)
-			playing = srect.collidepoint(pygame.mouse.get_pos())
+			playing = pygame.Rect(start.left, start.top, start.width, start.height).collidepoint(pygame.mouse.get_pos())
 
-			irect = pygame.Rect(367,393,115,22)
-			loadinstr = irect.collidepoint(pygame.mouse.get_pos())
+			inst = pygame.Rect(instructions.left, instructions.top, instructions.width, instructions.height).collidepoint(pygame.mouse.get_pos())
 
-			orect = pygame.Rect(383,423,85,22)
-			loadoptions = orect.collidepoint(pygame.mouse.get_pos())
+			ops = pygame.Rect(options.left, options.top, options.width, options.height).collidepoint(pygame.mouse.get_pos())
 
-			qrect = pygame.Rect(371,453,112,22)
-			quitz = qrect.collidepoint(pygame.mouse.get_pos())
+			quits = pygame.Rect(quit.left, quit.top, quit.width, quit.height).collidepoint(pygame.mouse.get_pos())
 
-			vrect = pygame.Rect(970,0,25,25)
-			mute = srect.collidepoint(pygame.mouse.get_pos())
+			vol = pygame.Rect(volume.left, volume.top, volume.width, volume.height).collidepoint(pygame.mouse.get_pos())
