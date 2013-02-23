069cd626b4ef3d0191a7df6b82d79c75d15476f3
mediocregopher <bgp2396@bellsouth.net>
2013-02-23 14:11:27 -0500

    added a special attack for thor... along with the transiententity class for that to use. Also, tabs got weird on me again, i'll figure this shit out eventually

--- character.py
+++ character.py
@@ -7,90 +7,100 @@ from pygame.sprite import Sprite
 
 class Character(LevelObject):
 
-    def __init__(self, x, y):
-        #general stuff
-        self.alive = True
-        self.isJumping = False   #used to detect the peak of player's jump
-        self.facingRight = True  #player facing right?
-        self.attacking = False   #player attacking?
-        self.velX = 0
-        self.velY = 0
+	def __init__(self, x, y):
+		#general stuff
+		self.alive = True
+		self.isJumping = False   #used to detect the peak of player's jump
+		self.facingRight = True  #player facing right?
+		self.attacking = False   #player attacking?
 
-        #load images and do rest of constructor
-        self.__populate_image_variables()
-        self.anim = None
-        self._load_image( self.stand )
-        LevelObject.__init__(self,x,y)
-    
 
-    def _load_image( self, img_tuple ):
-        left,right = img_tuple
-        toset = None
-        if self.facingRight: toset = right
-        else:                toset = left
+		#During an update it's possible the sprite changed, changing the rect size.
+		#In this case we need to update the character's rect object to reflect the new
+		#sprite size. However if the charspecificupdate already handled the changing
+		#rect size then we don't want to overwrite it afterwards, so in that case this
+		#should be set false
+		self.fixRectAfterUpdate = True
 
-        if not toset == self.anim:
-            toset.reset()
-        self.anim = toset
+		self.velX = 0
+		self.velY = 0
 
-    #updates the players velocities and animations
-    #orientation is used to track whether the character is facing left or right
-    def update(self):
-        self.anim.update()
+		#load images and do rest of constructor
+		self.__populate_image_variables()
+		self.anim = None
+		self._load_image( self.stand )
+		LevelObject.__init__(self,x,y)
+	
 
-        #If we're not alive don't process anything
-        if self.alive:
+	def _load_image( self, img_tuple ):
+		left,right = img_tuple
+		toset = None
+		if self.facingRight: toset = right
+		else:				toset = left
 
-            evman = eventmanager.get()
+		if not toset == self.anim:
+			toset.reset()
+		self.anim = toset
 
-            #replay
-            if evman.REPLAYPRESSED and logger.get().replayCanRun:
-                logger.get().replay()
+	#updates the players velocities and animations
+	#orientation is used to track whether the character is facing left or right
+	def update(self):
+		self.anim.update()
 
-            #Do any updates pertaining to the child character class.
-            #If the child class hasn't implemented the method, don't worry about it
-            try: self.charSpecificUpdate()
-            except AttributeError: pass
+		#If we're not alive don't process anything
+		if self.alive:
 
-        #update rect with new image
-        #we use bottomleft so it doesn't mess with collision detection
-        oldxy = self.rect.bottomleft
-        self.rect = self.anim.get_rect()
-        self.rect.bottomleft = oldxy
+			evman = eventmanager.get()
 
-        #Oh snap gravity!
-        self.velY += 1
-        self.rect.move_ip(self.velX,self.velY)
+			#replay
+			if evman.REPLAYPRESSED and logger.get().replayCanRun:
+				logger.get().replay()
 
-    def die(self):
-        self.velY = -15
-        self.stallX()
-        self.alive = False
-        self.solid = False
-        self._load_image( self.jump )
-        self.anim.blink = True
+			#Do any updates pertaining to the child character class.
+			#If the child class hasn't implemented the method, don't worry about it
+			try: self.charSpecificUpdate()
+			except AttributeError: pass
 
-    def stallX(self):
-        self.velX = 0
+		if self.fixRectAfterUpdate:
+			#update rect with new image
+			#we use bottomleft so it doesn't mess with collision detection
+			oldxy = self.rect.bottomleft
+			self.rect = self.anim.get_rect()
+			self.rect.bottomleft = oldxy
 
-    def stallY(self):
-        self.velY = 0
+		#Oh snap gravity!
+		self.velY += 1
+		self.rect.move_ip(self.velX,self.velY)
 
-    def stall(self):
-        self.stallX()
-        self.stallY()
+	def die(self):
+		self.velY = -15
+		self.stallX()
+		self.alive = False
+		self.solid = False
+		self._load_image( self.jump )
+		self.anim.blink = True
 
-    def __populate_image_variables(self):
-        animd = self.animFolder
-        self.norm_attack = StaticAnimation('images/' + animd + '/norm_attack_left.gif'),\
-                           StaticAnimation('images/' + animd + '/norm_attack_right.gif')
-        self.jump_attack = StaticAnimation('images/' + animd + '/jump_attack_left.gif'),\
-                           StaticAnimation('images/' + animd + '/jump_attack_right.gif')
-        #self.spec_attack = StaticAnimation(''),\
-        #                   StaticAnimation('')
-        self.jump        = StaticAnimation('images/' + animd + '/jump_left.gif'),\
-                           StaticAnimation('images/' + animd + '/jump_right.gif')
-        self.stand       = StaticAnimation('images/' + animd + '/stand_left.gif'),\
-                           StaticAnimation('images/' + animd + '/stand_right.gif')
-        self.walk        = Animation('images/' + animd + '/move_left{0}.gif',  self.numWalkFrames, self.walkDelay ),\
-                           Animation('images/' + animd + '/move_right{0}.gif', self.numWalkFrames, self.walkDelay )
+	def stallX(self):
+		self.velX = 0
+
+	def stallY(self):
+		self.velY = 0
+
+	def stall(self):
+		self.stallX()
+		self.stallY()
+
+	def __populate_image_variables(self):
+		animd = self.animFolder
+		self.norm_attack = StaticAnimation('images/' + animd + '/norm_attack_left.gif'),\
+						   StaticAnimation('images/' + animd + '/norm_attack_right.gif')
+		self.jump_attack = StaticAnimation('images/' + animd + '/jump_attack_left.gif'),\
+						   StaticAnimation('images/' + animd + '/jump_attack_right.gif')
+		#self.spec_attack = StaticAnimation(''),\
+		#				   StaticAnimation('')
+		self.jump		=  StaticAnimation('images/' + animd + '/jump_left.gif'),\
+						   StaticAnimation('images/' + animd + '/jump_right.gif')
+		self.stand	   =   StaticAnimation('images/' + animd + '/stand_left.gif'),\
+						   StaticAnimation('images/' + animd + '/stand_right.gif')
+		self.walk		=  Animation('images/' + animd + '/move_left{0}.gif',  self.numWalkFrames, self.walkDelay ),\
+						   Animation('images/' + animd + '/move_right{0}.gif', self.numWalkFrames, self.walkDelay )
--- player.py
+++ player.py
@@ -1,129 +1,183 @@
 from character import Character
 import eventmanager
 import sound
+from animation import StaticAnimation
+from levelobject import TransientEntity
+import pygame
 
 class Player(Character):
-    can_get_hurt  = True
-    can_give_hurt = True
-    attack_timer = 0
+	can_get_hurt  = True
+	can_give_hurt = True
+	attack_timer = 0
+	sattack_timer = 0
 
-    def charSpecificUpdate(self):
+	def __init__(self,x,y,level):
+		Character.__init__(self,x,y)
+		self.level = level
 
-        evman = eventmanager.get()
+	def charSpecificUpdate(self):
 
-        if evman.NORMPRESSED or self.attack_timer != 0: #normal attack pressed
+		evman = eventmanager.get()
 
-            if self.isJumping:
-                self._load_image( self.jump_attack )
-            else:
-                self._load_image( self.norm_attack )
+		if evman.NORMPRESSED or self.attack_timer != 0: #normal attack pressed
 
-            #mid attack
-            if self.attack_timer > 1:
-                self.attacking = True
-                self.attack_timer -= 1
-                self.velX = self.primary_attack_speed if self.facingRight else -1*self.primary_attack_speed
+			if self.isJumping:
+				self._load_image( self.jump_attack )
+			else:
+				self._load_image( self.norm_attack )
 
-            #start recovery phase
-            elif self.attack_timer == 1:
-                self.attacking = False
-                self.attack_timer = -1*self.primary_attack_recovery
-                if not self.isJumping: self.stallX()
+			#mid attack
+			if self.attack_timer > 1:
+				self.attacking = True
+				self.attack_timer -= 1
+				self.velX = self.primary_attack_speed if self.facingRight else -1*self.primary_attack_speed
 
-            #start attack
-            elif self.attack_timer == 0:
-                self.attacking = True
-                self.attack_timer = self.primary_attack_length
-                sound.play_sfx('sounds/SSB_Kick_Hit1.wav')
+			#start recovery phase
+			elif self.attack_timer == 1:
+				self.attacking = False
+				self.attack_timer = -1*self.primary_attack_recovery
+				if not self.isJumping: self.stallX()
 
-            #mid recovery
-            else:
-                self.attacking = False
-                self.attack_timer += 1
-                if not self.isJumping: self.stallX()
+			#start attack
+			elif self.attack_timer == 0:
+				self.attacking = True
+				self.attack_timer = self.primary_attack_length
+				sound.play_sfx('sounds/SSB_Kick_Hit1.wav')
 
-        elif evman.LEFTPRESSED: #left key pressed
-            self.velX = -self.runVel
-            self.facingRight = False
-            self._load_image( self.walk )
+			#mid recovery
+			else:
+				self.attacking = False
+				self.attack_timer += 1
+				if not self.isJumping: self.stallX()
 
-        elif evman.RIGHTPRESSED: #right key pressed
-            self.velX = self.runVel
-            self.facingRight = True
-            self._load_image( self.walk )
+		elif evman.SPECPRESSED or self.sattack_timer != 0:
+			self.special_attack()
 
-        else:
-            self.velX = 0
-            if not self.isJumping:
-                self._load_image( self.stand )
+		elif evman.LEFTPRESSED: #left key pressed
+			self.velX = -self.runVel
+			self.facingRight = False
+			self._load_image( self.walk )
 
-        #Attack animation overrides others
-        if not self.attacking:
-            if self.isJumping:
-                self._load_image( self.jump )
+		elif evman.RIGHTPRESSED: #right key pressed
+			self.velX = self.runVel
+			self.facingRight = True
+			self._load_image( self.walk )
 
-            #jumping upwards
-            elif evman.SPACEPRESSED:
-                self.isJumping = True
-                self.velY -= self.jumpVel
-                self._load_image( self.jump )
+		else:
+			self.velX = 0
+			if not self.isJumping:
+				self._load_image( self.stand )
 
-    def got_hurt(self,by):
-        self.die()
+		#Attack animation overrides others
+		if not self.attacking:
+			if self.isJumping:
+				self._load_image( self.jump )
+
+			#jumping upwards
+			elif evman.SPACEPRESSED:
+				self.isJumping = True
+				self.velY -= self.jumpVel
+				self._load_image( self.jump )
+
+	def got_hurt(self,by):
+		self.die()
 
 class CaptainAmerica(Player):
-    numWalkFrames = 4        #number pics in move anim
-    walkDelay = 2        #delay factor to make anims visible
+	numWalkFrames = 4		#number pics in move anim
+	walkDelay = 2		#delay factor to make anims visible
 
-    #movement vars
-    runVel = 10     #xcoord movement velocity
-    jumpVel = 25    #jumping velocity
+	#movement vars
+	runVel = 10	 #xcoord movement velocity
+	jumpVel = 25	#jumping velocity
 
-    animFolder = 'america'
+	animFolder = 'america'
 
-    primary_attack_speed = 10
-    primary_attack_length = 5
-    primary_attack_recovery = 5
+	primary_attack_speed = 10
+	primary_attack_length = 5
+	primary_attack_recovery = 5
 
 
 class Hulk(Player):
-    numWalkFrames = 4        #number pics in move anim
-    walkDelay = 2        #delay factor to make anims visible
+	numWalkFrames = 4		#number pics in move anim
+	walkDelay = 2		#delay factor to make anims visible
 
-    #movement vars
-    runVel = 25     #xcoord movement velocity
-    jumpVel = 35    #jumping velocity
+	#movement vars
+	runVel = 25	 #xcoord movement velocity
+	jumpVel = 35	#jumping velocity
 
-    animFolder = 'hulk'
+	animFolder = 'hulk'
 
-    primary_attack_speed = 10
-    primary_attack_length = 5
-    primary_attack_recovery = 5
+	primary_attack_speed = 10
+	primary_attack_length = 5
+	primary_attack_recovery = 5
 
 class IronMan(Player):
-    numWalkFrames = 4        #number pics in move anim
-    walkDelay = 5        #delay factor to make anims visible
+	numWalkFrames = 4		#number pics in move anim
+	walkDelay = 5		#delay factor to make anims visible
 
-    #movement vars
-    runVel = 7     #xcoord movement velocity
-    jumpVel = 25    #jumping velocity
+	#movement vars
+	runVel = 7	 #xcoord movement velocity
+	jumpVel = 25	#jumping velocity
 
-    animFolder = 'ironman'
+	animFolder = 'ironman'
 
-    primary_attack_speed = 10
-    primary_attack_length = 5
-    primary_attack_recovery = 5
+	primary_attack_speed = 10
+	primary_attack_length = 5
+	primary_attack_recovery = 5
 
 class Thor(Player):
-    numWalkFrames = 5        #number pics in move anim
-    walkDelay = 2        #delay factor to make anims visible
+	numWalkFrames = 5		#number pics in move anim
+	walkDelay = 2		#delay factor to make anims visible
 
-    #movement vars
-    runVel = 20     #xcoord movement velocity
-    jumpVel = 20    #jumping velocity
+	#movement vars
+	runVel = 20	 #xcoord movement velocity
+	jumpVel = 20	#jumping velocity
 
-    animFolder = 'thor'
+	animFolder = 'thor'
 
-    primary_attack_speed = 10
-    primary_attack_length = 5
-    primary_attack_recovery = 5
+	primary_attack_speed = 10
+	primary_attack_length = 5
+	primary_attack_recovery = 5
+
+	class ThorLightningLeft(TransientEntity):
+		attacking = True
+		can_give_hurt = True
+		base_img_path = 'images/thor/lightning_left.png'
+		timeout = 10
+
+	class ThorLightningRight(TransientEntity):
+		attacking = True
+		can_give_hurt = True
+		base_img_path = 'images/thor/lightning_right.png'
+		timeout = 10
+
+
+	def special_attack(self):
+		self._load_image( self.jump_attack )
+		self.stallX()
+		self.velY = -1 #counter gravity
+
+		if self.sattack_timer == 9:
+			if self.facingRight:
+				entity = self.ThorLightningRight(0,0)
+				entity.rect.topleft = self.rect.topright
+				self.level.addEntity(entity)
+			else:
+				entity = self.ThorLightningLeft(0,0)
+				entity.rect.topright = self.rect.topleft
+				self.level.addEntity(entity)
+			self.sattack_timer -= 1
+
+		#mid attack
+		elif self.sattack_timer > 1:
+			self.attacking = True
+			self.sattack_timer -= 1
+
+		#end of attack
+		elif self.sattack_timer == 1:
+			self.attacking = False
+			self.sattack_timer = 0
+
+		#start of attack
+		else:
+			self.sattack_timer = 10
