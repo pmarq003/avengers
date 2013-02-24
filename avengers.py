7bc08839bb9460ff1d41df4f903acbe360247396
j-a-c <runguy@ymail.com>
2013-02-24 13:39:39 -0500

    fixed broken char sel

--- avengers.py
+++ avengers.py
@@ -8,16 +8,12 @@ import time
 import logger
 import startmenu
 import sound
-import hud
 
 from pygame.locals import *
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT
 
 import os
 import sys
-
-#paused or not?
-isPaused = False
 
 #center screen
 os.environ['SDL_VIDEO_CENTERED'] = '1'
@@ -35,7 +31,6 @@ camera = camera.Camera(screen)
 currLevel = level.Level1()
 
 startMenu = startmenu.StartMenu()
-hud = hud.HUD()
 
 logger.get().set(camera, currLevel, screen, startMenu)
 
@@ -47,49 +42,43 @@ if "-m" in sys.argv:
 wasplaying = True #Hack to figure out when we need to change sounds
 while True:
 
-    #Start timer and handle events
-    milliStart = pygame.time.get_ticks()
-    events = pygame.event.get()
-    eventmanager.get().handleEvents(events)
-    logger.get().add(logger.LogNode(events))
+	#Start timer and handle events
+	milliStart = pygame.time.get_ticks()
+	events = pygame.event.get()
+	eventmanager.get().handleEvents(events)
+	logger.get().add(logger.LogNode(events))
 
+	if startMenu.isPlaying():
 
-    if startMenu.isPlaying():
+		if not wasplaying: sound.play_bgm(currLevel.bgm)
 
-        if not wasplaying: sound.play_bgm(currLevel.bgm)
+		if not currLevel.player_alive:
+			logger.get().clear()
+			currLevel = level.Level1()
 
-        if not currLevel.player_alive:
-            logger.get().clear()
-            currLevel = level.Level1()
+		#Update player and enemies positions/current actions
+		currLevel.update()
 
-        #Update player and enemies positions/current actions
-        currLevel.update()
+		#Update camera position using player's
+		player_rect = currLevel.get_player_rect()
+		camera.updatePosition(player_rect)
 
-        #Update camera position using player's
-        player_rect = currLevel.get_player_rect()
-        camera.updatePosition(player_rect)
+		#Fill the screen, draw level, flip the buffer
+		screen.fill(constants.DEFAULT_BGCOLOR)
+		currLevel.draw(camera)
+		wasplaying = True
 
-        currLevel.draw(camera)
+	else:
 
-        #Fill the screen, draw level, flip the buffer
-        screen.fill(constants.DEFAULT_BGCOLOR)
-        wasplaying = True
+		if wasplaying: sound.play_bgm(startMenu.bgm)
+		startMenu.update()
+		camera.zeroPosition()
+		startMenu.draw(camera)
+		wasplaying = False
 
-    else:
+	pygame.display.flip()
 
-        if wasplaying: sound.play_bgm(startMenu.bgm)
-        startMenu.update()
-        camera.zeroPosition()
-        startMenu.draw(camera)
-        wasplaying = False
-
-
-    hud.update()
-    hud.draw(camera)
-
-    pygame.display.flip()
-
-    #Stop timer and sleep for remainder of time
-    milliEnd = pygame.time.get_ticks()
-    leftover = constants.mSPF - (milliEnd - milliStart)
-    if leftover > 0: pygame.time.wait(int(leftover))
+	#Stop timer and sleep for remainder of time
+	milliEnd = pygame.time.get_ticks()
+	leftover = constants.mSPF - (milliEnd - milliStart)
+	if leftover > 0: pygame.time.wait(int(leftover))
